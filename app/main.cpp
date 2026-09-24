f9cbdc19586e8e775285724dd9b8fbe3ad3ffa47
Corentin Le Molgat <corentinl@google.com>
2026-09-24 14:17:17 +0200

    move config.h to lib

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,13 @@
 #include <QApplication>
+#include <QString>
 
 #include "window.h"
+#include "config.h"
 
 int main(int argc, char* argv[]) {
   QApplication app(argc, argv);
-  app.setOrganizationName("Mizux");
-  app.setApplicationName("DruxLord");
+  app.setOrganizationName(QString::fromUtf8(std::string{kProgramAuthorName}));
+  app.setApplicationName(QString::fromUtf8(std::string{kProgramName}));
 
   MainWindow main;
   main.show();
